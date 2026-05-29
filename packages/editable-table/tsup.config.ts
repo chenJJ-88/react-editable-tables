2853df68d90aa07733f8f03d8cd37bb248aa95b6
chenJJ-88 <118970817+chenJJ-88@users.noreply.github.com>
2026-05-29 16:32:12 +0800

    release v0.1.3,fix tsup dts config

--- packages/editable-table/tsup.config.ts
+++ packages/editable-table/tsup.config.ts
@@ -3,11 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['src/components/EditableTable/index.tsx'],
     format: ['esm'],
-    dts: {
-        compilerOptions: {
-            ignoreDeprecations: '6.0',
-        },
-    },
+    dts: true,
     clean: true,
     external: ['react', 'react-dom', '@tanstack/react-virtual'],
 });
--- packages/fast-editable-table/tsup.config.ts
+++ packages/fast-editable-table/tsup.config.ts
@@ -3,11 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['src/index.ts'],
     format: ['esm'],
-    dts: {
-        compilerOptions: {
-            ignoreDeprecations: '6.0',
-        },
-    },
+    dts: true,
     clean: true,
     external: ['react', 'react-dom', 'antd', '@formily/core', '@formily/react', '@formily/reactive'],
 });
